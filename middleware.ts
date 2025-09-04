7cb7c1db63d29ca4266f2a5ddfc79cd1ba94a3b9
Ben <thianlianben@coosy.co.jp>
2025-09-04 13:44:09 +0700

    update

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,5 @@ export const config = {
   // matcher: [
   //   '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   // ],
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-  runtime: 'nodejs',
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
