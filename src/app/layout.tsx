ea3ed66f58f773eb67e7c709c8d6e75c7c6c1fcc
Ben <thianlianben@coosy.co.jp>
2025-09-04 15:11:42 +0700

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next.js Supabase Auth',
+  title: 'Quest Genie',
   description: 'Next.js app with Supabase authentication',
 }
 
